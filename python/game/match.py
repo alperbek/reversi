9c4159c15cef76834e43c036299fb24d3577cd99
Naoki Shibuya <naoki.quant@gmail.com>
2016-08-16 20:39:43 +0800

    [Enhancement] Player's made hybrid of functional and object oriented programming.
    Minimax with/without alpha/beta are explained in comments.

--- python/game/match.py
+++ python/game/match.py
@@ -1,14 +1,24 @@
+import time
+
+
 def simple_match(context):
     """ A simple implementation of turn-based game match
     """
     print("Game start")
+    context.summary()
+
     turn = 1
-    # run while the game is active
     while context.is_active():
         player = context.get_player()
-        action = player(context)
+        print_progress(turn, player)
+        action = player.decide(context)
         context = context.apply(action)
-        print('Turn: {}'.format(turn))
         context.summary()
         turn += 1
+
     print("Game end")
+
+
+def print_progress(turn, player):
+    print('[{}] Turn: {} ({})'.format(
+        time.strftime("%H:%M:%S", time.localtime()), turn, player))
--- python/game/player.py
+++ python/game/player.py
@@ -3,34 +3,46 @@ import random
 INFINITY = float('Inf')
 
 
-def random_player(context):
-    actions = context.get_valid_actions()
-    if len(actions) == 0:
+class Player(object):
+    def decide(self, context):
+        """ Returns an action to take (or None to skip)
+        """
         return None
-    return random.choice(actions)
+
+    def __str__(self):
+        return self.__class__.__name__
 
 
-def manual_player(context):
-    valid_actions = context.get_valid_actions()
-    if len(valid_actions) == 0:
-        return None
-    while True:
-        command = raw_input('Enter a move row, col: ')
-        if command == 'quit':
-            exit(0)
-        elif command == 'show':
-            print('Valid moves: {}'.format(valid_actions))
-        elif command == 'help':
-            print('quit: terminate the match')
-            print('help: show this help')
-        else:
-            try:
-                action = eval(command)
-            except Exception as e:
-                print('Invalid input: {}'.format(e))
-            if action in valid_actions:
-                return action
-            print('Invalid move: {}'.format(action))
+class RandomPlayer(Player):
+    def decide(self, context):
+        actions = context.get_valid_actions()
+        if len(actions) == 0:
+            return None
+        return random.choice(actions)
+
+
+class ManualPlayer(Player):
+    def decide(self, context):
+        valid_actions = context.get_valid_actions()
+        if len(valid_actions) == 0:
+            return None
+        while True:
+            command = raw_input('Enter a move row, col: ')
+            if command == 'quit':
+                exit(0)
+            elif command == 'show':
+                print('Valid moves: {}'.format(valid_actions))
+            elif command == 'help':
+                print('quit: terminate the match')
+                print('help: show this help')
+            else:
+                try:
+                    action = eval(command)
+                except Exception as e:
+                    print('Invalid input: {}'.format(e))
+                if action in valid_actions:
+                    return action
+                print('Invalid move: {}'.format(action))
 
 
 def val_max(seq, fn):
@@ -52,54 +64,95 @@ def arg_max(seq, fn):
     return seq[values.index(max(values))]
 
 
-def minimax_player(root_context, max_depth):
-    def max_player(context, depth):
-        if not context.is_active() or depth > max_depth:
+class MinimaxPlayer(Player):
+    """ Naive minimax player
+
+    Minimax player iterate though all the valid moves to find the best (highest value) move.
+    For each, it needs to evaluate all the possible following moves by the opponent
+    who tries to choose the best move which is the worst move for this player.
+    This traversing of possible move chains will be continued until we reach the max depth
+    or there is no more move for both players.
+    """
+    def __init__(self, max_depth):
+        self._max_depth = max_depth
+
+    def decide(self, context):
+        return arg_max(context.get_valid_actions(),
+                       lambda action: self._min_play(context.apply(action), 1))
+
+    def _max_play(self, context, depth):
+        if not context.is_active() or depth > self._max_depth:
             return context.get_score()
         return val_max(context.get_valid_actions(),
-                       lambda action: min_player(context.apply(action), depth+1))
+                       lambda action: self._min_play(context.apply(action), depth+1))
 
-    def min_player(context, depth):
-        if not context.is_active() or depth > max_depth:
+    def _min_play(self, context, depth):
+        if not context.is_active() or depth > self._max_depth:
             return context.get_score()
         return val_min(context.get_valid_actions(),
-                       lambda action: max_player(context.apply(action), depth+1))
-
-    return arg_max(root_context.get_valid_actions(),
-                   lambda action: min_player(root_context.apply(action), 1))
+                       lambda action: self._max_play(context.apply(action), depth+1))
 
 
-def minimax_alpha_beta_player(root_context, max_depth):
-    def max_player(context, alpha, beta, depth):
-        if not context.is_active() or depth > max_depth:
+class MinimaxAlphaBetaPlayer(Player):
+    """ An improved version of minimax player.
+
+    The player (=max player) will choose the best of the worst moves chosen by the
+    min player, and the opponent (=min player) will choose the worst of the best
+    moves chosen by the max player.
+
+    The max player can stop traversing child nodes if the current node value is already
+    bigger than other sibling nodes (that were evaluated before that node) as the min
+    player will not choose the node anyway.
+
+    The min player can stop traversing child nodes if the current node value is already
+    smaller than other sibling nodes (that were evaluated before that node) as the max
+    player will not choose the node anyway.
+
+    Alpha is the best of the worst values chosen by the min player from the child nodes.
+    If the min player finds that the current node value is less than the alpha, it stops.
+
+    Beta is the worst of the best values chosen by the max player from the child nodes.
+    If the max player finds that the current node value is more than the beta, it stops.
+    """
+    def __init__(self, max_depth):
+        self._max_depth = max_depth
+
+    def decide(self, context):
+        return arg_max(context.get_valid_actions(),
+                       lambda action: self._min_play(context.apply(action),
+                                                     -INFINITY, INFINITY, 1))
+
+    def _max_play(self, context, alpha, beta, depth):
+        if not context.is_active() or depth > self._max_depth:
             return context.get_score()
         actions = context.get_valid_actions()
         if len(actions) == 0:
-            return min_player(context.apply(None), alpha, beta, depth)
+            return self._min_play(context.apply(None),
+                                  alpha, beta, depth)
         value = -INFINITY
         for action in actions:
-            value = max(value, min_player(context.apply(action), alpha, beta, depth+1))
+            value = max(value, self._min_play(context.apply(action),
+                                              alpha, beta, depth+1))
             if value >= beta:
                 return value
             alpha = max(alpha, value)
         return value
 
-    def min_player(context, alpha, beta, depth):
-        if not context.is_active() or depth > max_depth:
+    def _min_play(self, context, alpha, beta, depth):
+        if not context.is_active() or depth > self._max_depth:
             return context.get_score()
         actions = context.get_valid_actions()
         if len(actions) == 0:
-            return max_player(context.apply(None), alpha, beta, depth)
+            return self._max_play(context.apply(None),
+                                  alpha, beta, depth)
         value = INFINITY
         for action in actions:
-            value = min(value, max_player(context.apply(action), alpha, beta, depth+1))
+            value = min(value, self._max_play(context.apply(action),
+                                              alpha, beta, depth+1))
             if value <= alpha:
                 return value
             beta = min(beta, value)
         return value
-
-    return arg_max(root_context.get_valid_actions(),
-                   lambda action: min_player(root_context.apply(action), -INFINITY, INFINITY, 1))
 
 
 def choose_player(message):
@@ -112,14 +165,14 @@ def choose_player(message):
         try:
             number = eval(raw_input('Enter [1-4]: '))
             if number == 1:
-                return random_player
+                return RandomPlayer()
             if number == 2:
                 number = eval(raw_input('Max Depth: '))
-                return lambda context: minimax_player(context, number)
+                return MinimaxPlayer(number)
             if number == 3:
                 number = eval(raw_input('Max Depth: '))
-                return lambda context: minimax_alpha_beta_player(context, number)
+                return MinimaxAlphaBetaPlayer(number)
             if number == 4:
-                return manual_player
+                return ManualPlayer()
         except Exception as e:
             print(e)
